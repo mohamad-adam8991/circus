6ec4e56e06174e56048141299e5924a50aeb6e3c
mohamad-adam8991 <69377976+mohamad-adam8991@users.noreply.github.com>
2021-09-10 15:19:01 +0800

    Add all changes done in the lecture

--- src/main/java/circus/animal/Duck.java
+++ src/main/java/circus/animal/Duck.java
@@ -1,7 +1,14 @@
+package circus.animal;
+
 public class Duck extends Bird {
     @Override
     public String speak() {
         return "Quack Quack";
+    }
+
+    @Override
+    public int getValue() {
+        return 10;
     }
 
     @Override
@@ -12,4 +19,5 @@ public class Duck extends Bird {
     public void swim() {
         System.out.println("I'm swimming...");
     }
+
 }
